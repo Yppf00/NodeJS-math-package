3f017854c0cb6b554594de2b7335796401bce8ff
[ritual] ../*.shen <106227931+Yppf00@users.noreply.github.com>
2022-06-28 12:39:31 +0200

    Add files via upload

---
+++ New folder/math.js
@@ -0,0 +1,6 @@
+let add = (a, b) => a + b
+let substract = (a, b) => a - b
+let multiply = (a, b) => a * b
+let divine = (a, b) => a / b
+
+module.exports = { add, substract, divine, multiply }
---
+++ New folder/usage.js
@@ -0,0 +1,4 @@
+//add math package to your project
+
+let math = require('./math')
+console.log(math.add(2, 3))
